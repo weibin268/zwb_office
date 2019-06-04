d9aa77982070dcef27e9bc441904a421acf422a7
zwb <zhuangweibin@stbl.cc>
2019-06-04 18:42:42 +0800

    first commit

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="zwb_office",
+    version="1.0.0",
+    keywords=("util"),
+    description="zwb_office",
+    long_description="zwb_office",
+    url="https://github.com/weibin268/zwb_office",
+    author="zhuang weibin",
+    author_email="448075543@qq.com",
+    packages=find_packages(),
+    include_package_data=True,
+    platforms="any",
+    install_requires=[]
+)
